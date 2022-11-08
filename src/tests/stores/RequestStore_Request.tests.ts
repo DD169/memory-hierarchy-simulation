42dbf4c50b88ae63b07a8732b3776704e406df82
Dante <delariva@etsu.edu>
2022-11-08 13:03:38 -0500

    Test the Request Class

--- src/tests/stores/RequestStore_Request.tests.ts
+++ src/tests/stores/RequestStore_Request.tests.ts
@@ -1,9 +1,11 @@
 import { Request } from "../../stores/RequestStore";
 
 test("should construct object properly", () => {
-  const request = new Request(1234, "R", true);
+  const request: Request = new Request(1234, "W", true);
+  expect(request).toEqual({ addr: 1234, mode: "W", isCompleted: true });
+});
 
-  expect(request.addr).toBe(1234);
-  expect(request.mode).toBe("R");
-  expect(request.isCompleted).toBe(true);
+test("should emit proper toString", () => {
+  const request: Request = new Request(1234, "W", true);
+  expect(request.toString()).toBe("W:1234");
 });
